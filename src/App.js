c04a0b1e116148026b203b98e1949dd9b07a410a
Pranav Viswanath <pviswanath04@gmail.com>
2023-08-04 15:17:20 -0400

    created banner and exhibit component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // Define the Exhibit component as a nested component
+  function Exhibit({ heading, children }) {
+    return (
+      <div className="Exhibit">
+        <h2>{heading}</h2>
+        <div className="Exhibit-content">
+          {children}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Use the Exhibit component */}
+        <Exhibit heading="Sample Exhibit">
+          <p>This is the content of the exhibit.</p>
+          <button>Click Me</button>
+          {/* You can add more child components here */}
+        </Exhibit>
       </header>
     </div>
   );
