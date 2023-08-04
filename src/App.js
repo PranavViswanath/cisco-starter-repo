6b6c0f059dabe4d788ed834a7f90f786225b003a
Pranav Viswanath <pviswanath04@gmail.com>
2023-08-04 18:32:00 -0400

    created IP address component in exhibit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  // Define the Exhibit component as a nested component
-  function Exhibit({ heading, children }) {
-    return (
-      <div className="Exhibit">
-        <h2>{heading}</h2>
-        <div className="Exhibit-content">
-          {children}
-        </div>
+function Exhibit({ heading, children }) {
+  return (
+    <div className="Exhibit">
+      <h2>{heading}</h2>
+      <div className="Exhibit-content">
+        {children}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
+function IPAddressComponent({ ipVersion }) {
+  const [ipAddress, setIPAddress] = useState('');
+
+  useEffect(() => {
+    // Fetch the IP address from the ipify API
+    fetch(`https://api64.ipify.org?format=json&${ipVersion}`)
+      .then(response => response.json())
+      .then(data => setIPAddress(data.ip))
+      .catch(error => console.error('Error fetching IP address:', error));
+  }, [ipVersion]);
+
+  return (
+    <div>
+      <p>Your public {ipVersion} address is: {ipAddress}</p>
+    </div>
+  );
+}
+
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Use the Exhibit component */}
-        <Exhibit heading="Sample Exhibit">
-          <p>This is the content of the exhibit.</p>
-          <button>Click Me</button>
-          {/* You can add more child components here */}
+        <Exhibit heading="IPv4 Address">
+          <IPAddressComponent ipVersion="ipv4" />
+        </Exhibit>
+        <Exhibit heading="IPv6 Address">
+          <IPAddressComponent ipVersion="ipv6" />
         </Exhibit>
       </header>
     </div>
